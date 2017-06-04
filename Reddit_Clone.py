6d180fd182e475ef9e1e858b3330fbffd035d901
Karthik-Ramvijay <karthikthenew@gmail.com>
2017-06-05 07:50:40 +0800

    Logic for Sorting Topic
    Added the Sorting Logic by using the click event of Done Voting Button.

--- Reddit_Clone.py
+++ Reddit_Clone.py
@@ -39,5 +39,56 @@ def post():
             return output
 
 
+@route('/sorting',method=["POST"])
+def sort():
+    id=int(request.POST.get('click'))
+    name={}
+    for key,value in request.forms.items():
+        name[key]=int(value)
+
+    del name['click']
+
+    if id==1:
+
+        new_list=mydict.values()
+        for key in name.keys():
+            for tem in new_list:
+                if key == tem[1]:
+                    tem.append(name[key])
+
+        my_list=sorted(new_list, key=lambda x:x[2],reverse=True)
+        table=PrettyTable()
+        table.field_names=['Topic','Votes']
+
+        for item in my_list[:20]:
+            table.add_row([item[0],item[2]])
+
+        table_html=table.get_html_string()
+        return table_html
+
+    elif id > 1:
+        new_dict={}
+
+        for value in mydict.values():
+            try:
+                new_dict[value[1]]=[value[0],value[2]]
+            except Exception:
+                new_dict[value[1]]=[value[0],name[value[1]]]
+
+        for key in name.keys():
+            new_dict[key][1]=name[key]
+
+        my_list=sorted(new_dict.values(), key=lambda x:x[1],reverse=True)
+
+        table=PrettyTable()
+        table.field_names=['Topic','Votes']
+
+        for item in my_list[:20]:
+            table.add_row([item[0],item[1]])
+
+        table_html=table.get_html_string()
+        return table_html
+
+
 if __name__ == '__main__':
     run(host=HOST,debug=True,reloader=True)

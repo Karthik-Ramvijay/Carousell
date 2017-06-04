c23e4eabb9d790c24667c63dff34ccee2390192b
Karthik-Ramvijay <karthikthenew@gmail.com>
2017-06-05 00:09:22 +0800

    Method For Routing the Post Method
    This method will get the table values in dictionary and html tag is
    formed to return the table rows dynamically

--- Reddit_Clone.py
+++ Reddit_Clone.py
@@ -13,6 +13,27 @@ def server_static(filepath):
 def index():
    return template('index.tpl',rows=mydict)
 
+@route('/',method="POST")
+def post():
+        topic=request.POST.get('topictxt')
+        if topic.strip():
+            key=len(mydict)+1
+            txt=topic.strip()
+            label_count="count_"+str(key)
+            mydict[key]=[txt,label_count]
+            table_content="<tr>"\
+                       "<td>"\
+                        +txt+\
+                        "</td>"\
+                        "<td><input class=upbutton value=UP type=image  id="+label_count+" src=static/Picture1.png>"\
+                        "</td>"\
+                        "<td width=3 height=5><input name="+label_count+" id="+label_count+" value=0 size=3 readonly></input></td>"\
+						"<td><input class=downbutton value=DOWN type=image  id="+label_count+"  src=static/Picture2.png>"\
+						"</button></td>"\
+						"</tr>"
+
+            return table_content
+
 
 if __name__ == '__main__':
     run(host=HOST,debug=True,reloader=True)
